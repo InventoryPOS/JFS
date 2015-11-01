72356299ecde9080ada079d5c4274b6242096920
jordanacortesm <jordan.andreicm@gmail.com>
2015-11-01 11:34:39 -0500

    Se añade proyecto con navegacion exitosa
    Se añade proyecto con navegacion exitosa

---
+++ Desarrollo/InventoryPOSPresentacion/src/java/co/com/inventorypos/presentacion/web/UsuarioBeanVista.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.inventorypos.presentacion.web;
+
+/**
+ *
+ * @author V5
+ */
+public class UsuarioBeanVista {
+    private String nombre;
+    private String contrasena;
+
+    /**
+     * Atributo Nombre
+     * @return 
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    /**
+     * Atributo Contraseña
+     * @return 
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    
+}
