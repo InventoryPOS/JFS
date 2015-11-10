fa05574976b0ca81fae6b22aa8713bf0e809fff4
nanomora <fernando.morac@hotmail.com>
2015-11-10 00:26:08 -0500

    test de negocio

--- Desarrollo/InventoryPOSNegocio/test/co/com/inventorypos/negocio/impl/IPosNegocioFachadaTest.java
+++ Desarrollo/InventoryPOSNegocio/test/co/com/inventorypos/negocio/impl/IPosNegocioFachadaTest.java
@@ -5,7 +5,6 @@
  */
 package co.com.inventorypos.negocio.impl;
 
-import co.com.inventorypos.comun.enums.EnumFuncionalidades;
 import co.com.inventorypos.comun.enums.EnumPerfil;
 import co.com.inventorypos.comun.vo.InsumoVO;
 import co.com.inventorypos.negocio.IIPosNegocioFachada;
@@ -13,7 +12,6 @@ import java.util.List;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -56,9 +54,7 @@ public class IPosNegocioFachadaTest {
         System.out.println("getInstancia");
         IIPosNegocioFachada expResult = null;
         IIPosNegocioFachada result = IPosNegocioFachada.getInstancia();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotSame(expResult, result);
     }
 
     /**
@@ -69,28 +65,13 @@ public class IPosNegocioFachadaTest {
         System.out.println("verificarCredenciales");
         String usuario = "";
         String password = "";
-        IPosNegocioFachada instance = null;
         EnumPerfil expResult = null;
-        EnumPerfil result = instance.verificarCredenciales(usuario, password);
+        EnumPerfil result = negocioFachada.verificarCredenciales(usuario, password);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of getFuncionalidades method, of class IPosNegocioFachada.
-     */
-    @org.junit.Test
-    public void testGetFuncionalidades() throws Exception {
-        System.out.println("getFuncionalidades");
-        EnumPerfil perfil = null;
-        IPosNegocioFachada instance = null;
-        List<EnumFuncionalidades> expResult = null;
-        List<EnumFuncionalidades> result = instance.getFuncionalidades(perfil);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of getInsumos method, of class IPosNegocioFachada.
@@ -98,12 +79,9 @@ public class IPosNegocioFachadaTest {
     @org.junit.Test
     public void testGetInsumos_0args() throws Exception {
         System.out.println("getInsumos");
-        IPosNegocioFachada instance = null;
-        List<InsumoVO> expResult = null;
-        List<InsumoVO> result = instance.getInsumos();
-        assertEquals(expResult, result);
+        List<InsumoVO> result = negocioFachada.getInsumos();
+        assertNotSame(0, result.size());
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -112,40 +90,12 @@ public class IPosNegocioFachadaTest {
     @org.junit.Test
     public void testGetInsumos_String_String() throws Exception {
         System.out.println("getInsumos");
-        String codigo = "";
-        String nombre = "";
-        IPosNegocioFachada instance = null;
-        List<InsumoVO> expResult = null;
-        List<InsumoVO> result = instance.getInsumos(codigo, nombre);
-        assertEquals(expResult, result);
+        String codigo = null;
+        String nombre = "1";
+        List<InsumoVO> result = negocioFachada.getInsumos(codigo, nombre);
+        assertEquals(1, result.size());
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of actualizarInsumo method, of class IPosNegocioFachada.
-     */
-    @org.junit.Test
-    public void testActualizarInsumo() throws Exception {
-        System.out.println("actualizarInsumo");
-        InsumoVO insumo = null;
-        IPosNegocioFachada instance = null;
-        instance.actualizarInsumo(insumo);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of crearInsumo method, of class IPosNegocioFachada.
-     */
-    @org.junit.Test
-    public void testCrearInsumo() throws Exception {
-        System.out.println("crearInsumo");
-        InsumoVO insumo = null;
-        IPosNegocioFachada instance = null;
-        instance.crearInsumo(insumo);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
